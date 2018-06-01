3e74f0e0c846148d87a40347e13789cbd436d9bb
Daniel Céspedes <dacesmo@gmail.com>
2018-06-01 14:39:56 -0600

    added a script to wakeup ssh_keys

---
+++ terransible/wake_up_keys.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function wake_up() {
+  eval `ssh-agent -s`
+  /bin/ssh-add ~/.ssh/fruit
+  /bin/ssh-add -l
+}
+
+/bin/ssh-add -l 2&>1
+if ! [ $? -eq 0 ]
+then
+  wake_up;
+fi;
+
+/bin/ssh-add -l
+
+exit 0;
