46de43d7cecc5795d400a1b2ce6f612215a1884d
Daniel Céspedes <dacesmo@gmail.com>
2018-06-03 15:54:04 -0600

    maven build added for Hygieia

--- terransible/main.tf
+++ terransible/main.tf
@@ -5,89 +5,89 @@ provider "aws" {
 
 #------ VPC ------
 
-resource "aws_vpc" "jenkins_hygieia_env_vpc" {
+resource "aws_vpc" "jenkins_env_vpc" {
   cidr_block           = "${var.vpc_cidr}"
 /*  enable_dns_hostnames = false
   enable_dns_support   = false*/
 
   tags {
-    Name = "jenkins_hygieia_env_vpc"
+    Name = "jenkins_env_vpc"
   }
 
 }
 
 #internet gateway
 
-resource "aws_internet_gateway" "jenkins_hygieia_env_internet_gateway" {
-  vpc_id = "${aws_vpc.jenkins_hygieia_env_vpc.id}"
+resource "aws_internet_gateway" "jenkins_env_internet_gateway" {
+  vpc_id = "${aws_vpc.jenkins_env_vpc.id}"
 
   tags {
-    Name = "jenkins_hygieia_env_igw"
+    Name = "jenkins_env_igw"
   }
 }
 
 #route tables
 
-resource "aws_route_table" "jenkins_hygieia_env_public_rt" {
-  vpc_id = "${aws_vpc.jenkins_hygieia_env_vpc.id}"
+resource "aws_route_table" "jenkins_env_public_rt" {
+  vpc_id = "${aws_vpc.jenkins_env_vpc.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.jenkins_hygieia_env_internet_gateway.id}"
+    gateway_id = "${aws_internet_gateway.jenkins_env_internet_gateway.id}"
   }
 
   tags {
-    Name = "jenkins_hygieia_public"
+    Name = "jenkins_public"
   }
 }
 
-/*resource "aws_default_route_table" "jenkins_hygieia_private_rt" {
-  default_route_table_id = "${aws_vpc.jenkins_hygieia_env_vpc.default_route_table_id}"
+/*resource "aws_default_route_table" "jenkins_private_rt" {
+  default_route_table_id = "${aws_vpc.jenkins_env_vpc.default_route_table_id}"
 
   tags {
-    Name = "jenkins_hygieia_private"
+    Name = "jenkins_private"
   }
 }*/
 
 # Subnets declaration
 
-resource "aws_subnet" "jenkins_hygieia_env_public1_subnet" {
-  vpc_id                  = "${aws_vpc.jenkins_hygieia_env_vpc.id}"
+resource "aws_subnet" "jenkins_env_public1_subnet" {
+  vpc_id                  = "${aws_vpc.jenkins_env_vpc.id}"
   cidr_block              = "${var.cidrs["public1"]}"
   map_public_ip_on_launch = true
   availability_zone       = "${data.aws_availability_zones.available.names[0]}"
 
   tags {
-    Name = "jenkins_hygieia_public1"
+    Name = "jenkins_public1"
   }
 }
 
-/*resource "aws_subnet" "jenkins_hygieia_private1_subnet" {
-  vpc_id                  = "${aws_vpc.jenkins_hygieia_env_vpc.id}"
+/*resource "aws_subnet" "jenkins_private1_subnet" {
+  vpc_id                  = "${aws_vpc.jenkins_env_vpc.id}"
   cidr_block              = "${var.cidrs["private1"]}"
   map_public_ip_on_launch = false
   availability_zone       = "${data.aws_availability_zones.available.names[0]}"
 
   tags {
-    Name = "jenkins_hygieia_private1"
+    Name = "jenkins_private1"
   }
 }*/
 
 # Subnet asociations
 
-resource "aws_route_table_association" "jenkins_hygieia_env_public1_assoc" {
-  subnet_id      = "${aws_subnet.jenkins_hygieia_env_public1_subnet.id}"
-  route_table_id = "${aws_route_table.jenkins_hygieia_env_public_rt.id}"
+resource "aws_route_table_association" "jenkins_env_public1_assoc" {
+  subnet_id      = "${aws_subnet.jenkins_env_public1_subnet.id}"
+  route_table_id = "${aws_route_table.jenkins_env_public_rt.id}"
 }
 
 #Public Security Group
 
-resource "aws_security_group" "jenkins_hygieia_env_public_sg" {
-  name        = "jenkins_hygieia_env_public_sg"
-  description = "Used for public access to jenkins_hygieia and jenkins_hygieia"
-  vpc_id      = "${aws_vpc.jenkins_hygieia_env_vpc.id}"
+resource "aws_security_group" "jenkins_env_public_sg" {
+  name        = "jenkins_env_public_sg"
+  description = "Used for public access to jenkins and jenkins"
+  vpc_id      = "${aws_vpc.jenkins_env_vpc.id}"
 
-  #jenkins_hygieia
+  #jenkins
 
   /*ingress {
     from_port   = 8081
@@ -122,10 +122,10 @@ resource "aws_security_group" "jenkins_hygieia_env_public_sg" {
   }
 }
 
-/*resource "aws_security_group" "jenkins_hygieia_private_sg" {
-  name        = "jenkins_hygieia_private_sg"
+/*resource "aws_security_group" "jenkins_private_sg" {
+  name        = "jenkins_private_sg"
   description = "Used for access to private instances"
-  vpc_id      = "${aws_vpc.jenkins_hygieia_env_vpc.id}"
+  vpc_id      = "${aws_vpc.jenkins_env_vpc.id}"
 
   #All VPC
 
@@ -147,63 +147,71 @@ resource "aws_security_group" "jenkins_hygieia_env_public_sg" {
 
 #Key Pair
 
-resource "aws_key_pair" "jenkins_hygieia_env_auth" {
+resource "aws_key_pair" "jenkins_env_auth" {
   key_name   = "${var.key_name}"
   public_key = "${file(var.public_key_path)}"
 }
 
 #Jenkins ec2
 
-resource "aws_instance" "jenkins_hygieia_ec2" {
+resource "aws_instance" "jenkins_ec2" {
   instance_type = "${var.ec2_instance_type}"
   ami           = "${var.ec2_ami}"
 
+  root_block_device {
+        volume_size = 30
+    }
+
   tags {
-    Name = "jenkins_hygieia_ec2"
+    Name = "jenkins_ec2"
   }
 
-  key_name               = "${aws_key_pair.jenkins_hygieia_env_auth.id}"
-  vpc_security_group_ids = ["${aws_security_group.jenkins_hygieia_env_public_sg.id}"]
-  subnet_id              = "${aws_subnet.jenkins_hygieia_env_public1_subnet.id}"
+  key_name               = "${aws_key_pair.jenkins_env_auth.id}"
+  vpc_security_group_ids = ["${aws_security_group.jenkins_env_public_sg.id}"]
+  subnet_id              = "${aws_subnet.jenkins_env_public1_subnet.id}"
 
   provisioner "local-exec" {
     command = <<EOD
 cat <<EOF >> aws_hosts
 [jenkins]
-${aws_instance.jenkins_hygieia_ec2.public_ip}
+${aws_instance.jenkins_ec2.public_ip}
 EOF
 EOD
   }
 
   provisioner "local-exec" {
-    command = "aws ec2 wait instance-status-ok --instance-ids ${aws_instance.jenkins_hygieia_ec2.id} --profile ${var.aws_profile} && export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -i aws_hosts hygieia.yml"
+    command = "aws ec2 wait instance-status-ok --instance-ids ${aws_instance.jenkins_ec2.id} --profile ${var.aws_profile} && export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -i aws_hosts jenkins.yml"
   }
 }
 
 #K8s ec2
 
-resource "aws_instance" "k8s_cluster_ec2" {
+resource "aws_instance" "hygieia_ec2" {
   instance_type = "${var.ec2_instance_type}"
   ami           = "${var.ec2_ami}"
 
+  root_block_device {
+        volume_size = 30
+    }
+
   tags {
-    Name = "k8s_cluster_ec2"
+    Name = "hygieia_ec2"
   }
 
-  key_name               = "${aws_key_pair.jenkins_hygieia_env_auth.id}"
-  vpc_security_group_ids = ["${aws_security_group.jenkins_hygieia_env_public_sg.id}"]
-  subnet_id              = "${aws_subnet.jenkins_hygieia_env_public1_subnet.id}"
+  key_name               = "${aws_key_pair.jenkins_env_auth.id}"
+  vpc_security_group_ids = ["${aws_security_group.jenkins_env_public_sg.id}"]
+  subnet_id              = "${aws_subnet.jenkins_env_public1_subnet.id}"
 
   provisioner "local-exec" {
     command = <<EOD
 cat <<EOF >> aws_hosts
-[jenkins]
-${aws_instance.k8s_cluster_ec2.public_ip}
+[hygieia]
+${aws_instance.hygieia_ec2.public_ip}
 EOF
 EOD
   }
 
   provisioner "local-exec" {
-    command = "aws ec2 wait instance-status-ok --instance-ids ${aws_instance.k8s_cluster_ec2.id} --profile ${var.aws_profile} && export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -i aws_hosts hygieia.yml"
+    command = "aws ec2 wait instance-status-ok --instance-ids ${aws_instance.hygieia_ec2.id} --profile ${var.aws_profile} && export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -i aws_hosts hygieia.yml"
   }
 }
